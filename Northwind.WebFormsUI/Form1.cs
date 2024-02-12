75b5507e83addfc73da613dbebfbd514bd897292
Onur Aydı <onuraydi@outlook.com>
2024-02-12 14:34:04 +0300

    Dependency Injection and lOC container implementation

--- Northwind.WebFormsUI/Form1.cs
+++ Northwind.WebFormsUI/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Northwind.Business.Abstract;
 using Northwind.Business.Concrete;
+using Northwind.Business.DependencyResolvers.Ninject;
 using Northwind.DataAccess.Abstract;
 using Northwind.DataAccess.Concrete.EntityFramework;
 using Northwind.DataAccess.Concrete.NHibernate;
@@ -10,21 +11,21 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Northwind.WebFormsUI
+namespace Northwind.WebFormsUI 
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            _categoryService = new CategoryManager(new EfCategoryDal());
-            _productService =  new ProductManager(new EfProductDal());                                                                    
-        }     // bu şekilde efproductdal yerine nhproduct dal yazarsak da çalışacak bu kullanım(backend kısmı)
-              // programı farklı database'lere geçirmeyi kolaylaylaştırır. bağımlılıklar minize edilerek böyle oldu
+            _productService = InstanceFactory.GetInstance<IProductService>();
+            _categoryService = InstanceFactory.GetInstance<ICategoryService>();                                                                 
+        }    
 
         private IProductService _productService;
         private ICategoryService _categoryService;
